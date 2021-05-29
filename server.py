38960e6a88273208a76c3033c44c6558ea046818
Adwait Arajaria <adeveloper121@gmail.com>
2021-05-30 00:56:29 +0530

    Server now accepts data from one client and sends it to others

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
-import client
+import time
+import threading
 
 host = '0.0.0.0'
 port = 5407
@@ -10,18 +11,36 @@ client_list = []
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 def send_data (sender, data):
-    while True:
-        for c in client_list:
-            if c == sender:
-                continue
-            stream.sendall(data)
+    for c in client_list:
+        if c == sender:
+            continue
+        c.stream.sendall(data)
+
+            
+class ClientHandler (object):
+    
+    def __init__(self, stream, address):
+        self.stream = stream
+        self.address = address
+
+        t1 = threading.Thread(target=self.recieve_data, args=())
+        t1.start()
+
+    def recieve_data (self):
+        with self.stream:
+            while True:
+                data = self.stream.recv(1024)
+                if not data:
+                    break
+                send_data(self, data)
+                time.sleep(0.25)
 
 with s:
     s.bind((host, port))
     while len(client_list) < max_players:
         s.listen()
         stream, address = s.accept()
-        _client = client.ClientHandler(stream, address)
+        _client = ClientHandler(stream, address)
         client_list.append(_client)
         print('connection recieved from', address)
 

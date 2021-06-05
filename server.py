f4f63e261aee2339f54a95db90a27d552b567580
Adwait Arajaria <adeveloper121@gmail.com>
2021-06-06 03:53:10 +0530

    added udp handling

--- server.py
+++ server.py
@@ -5,10 +5,15 @@ import threading
 host = '0.0.0.0'
 port = 5407
 
+udp_port = 25011
+
 max_players = 2
 client_list = []
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+udps = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udps.bind((host, udp_port))
 
 def send_data (sender, data):
     for c in client_list:
@@ -16,15 +21,24 @@ def send_data (sender, data):
             continue
         c.stream.sendall(data)
 
-            
+def udpsend (sender, data):
+    for c in client_list:
+        if c == sender:
+            continue
+        udps.sendto(data, c.udpaddr)
+
 class ClientHandler (object):
     
     def __init__(self, stream, address):
         self.stream = stream
         self.address = address
+        self.udpaddr = address
 
         t1 = threading.Thread(target=self.recieve_data, args=())
         t1.start()
+
+        t_udp = threading.Thread(target=self.recieve_data_udp, args=())
+        t_udp.start()
 
     def recieve_data (self):
         with self.stream:
@@ -33,7 +47,14 @@ class ClientHandler (object):
                 if not data:
                     break
                 send_data(self, data)
-                time.sleep(0.03)
+                time.sleep(0.01)
+
+    def recieve_data_udp (self):
+        while True:
+            data, addr = udps.recvfrom(1024)
+            self.udpaddr = addr
+            udpsend(self, data)
+            time.sleep(0.01)
 
 with s:
     s.bind((host, port))
